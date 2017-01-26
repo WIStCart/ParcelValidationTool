d7414eea2ac6db8460ae6965e1887abf61a30647
djvogel03 <djvogel03@gmail.com>
2017-01-26 11:46:02 -0600

    addition to pinskip lists and tweaks to summary output

--- Summary.py
+++ Summary.py
@@ -14,12 +14,22 @@ class Summary:
 		Summary.errorSummaryFile.write("Validation Summary Table: " + "\n")
 		Summary.errorSummaryFile.write("This validation summary table contains an overview of any errors found by the Parcel Validation Tool. Please review the contents of this file and make changes to your parcel dataset as necessary." + "\n\n")
 		Summary.errorSummaryFile.write("************************************************************************\n")
-		Summary.errorSummaryFile.write("In-line errors - The following lines summarize the element-specific errors that were found while validating your parcel dataset. The stats below are meant as a means of reviewing the output. Please see the " + "GeneralElementErrors, AddressElementErrors, TaxrollElementErrors, and GeometricElementErrors fields in the output feature class to address these errors individually."+ "\n")
+		Summary.errorSummaryFile.write("Broad-level errors - The following lines summarize the element-specific errors that were found while validating your parcel dataset. The stats below are meant as a means of reviewing the output. Please see the " + "GeneralElementErrors, AddressElementErrors, TaxrollElementErrors, and GeometricElementErrors fields in the output feature class to address these errors individually."+ "\n")
 		Summary.errorSummaryFile.write("	General Errors: " + str(totError.generalErrorCount) + "\n")
 		Summary.errorSummaryFile.write("	Geometric Errors: " + str(totError.geometricErrorCount) + "\n")
 		Summary.errorSummaryFile.write("	Address Errors: " + str(totError.addressErrorCount) + "\n")
 		Summary.errorSummaryFile.write("	Tax Errors: " + str(totError.taxErrorCount) + "\n")
 		Summary.errorSummaryFile.write("\n\n")
+		Summary.errorSummaryFile.write("The following lines explain any broad geometric errors that were found while validating your parcel dataset."+ "\n")
+		if len(totError.geometricPlacementErrors) != 0:
+			for geometricErrorMessage in totError.geometricPlacementErrors:
+				Summary.errorSummaryFile.write("	General geometric error: " + str(geometricErrorMessage) + "\n")
+		if len(totError.geometricFileErrors) != 0:
+			for geometricErrorMessage in totError.geometricFileErrors:
+				Summary.errorSummaryFile.write("	General geometric error: " + str(geometricErrorMessage) + "\n")
+		if (len(totError.geometricFileErrors) == 0) and (len(totError.geometricPlacementErrors) == 0):
+			Summary.errorSummaryFile.write("	*No broad-level geometric errors found!" + "\n")
+		Summary.errorSummaryFile.write("\n\n")	
 		Summary.errorSummaryFile.write("Percentage of records with various Taxroll Years" + "\n")
 		Summary.errorSummaryFile.write("	Previous Taxroll Year: " + str(round((float(totError.trYearPast / float((totError.recordTotalCount - totError.pinSkipCount)))*100),2)) + "%\n")
 		Summary.errorSummaryFile.write("	Expected Taxroll Year: " + str(round((float(totError.trYearExpected / float((totError.recordTotalCount - totError.pinSkipCount)))*100),2)) + "%\n")
@@ -77,15 +87,7 @@ class Summary:
 		Summary.errorSummaryFile.write("	  PARCELSRC: 	" + str(totError.comparisonDict["PARCELSRC"]) + '\n')
 		Summary.errorSummaryFile.write("\n\n\n* Within: " + outDirTxt + "\\" + outName  + "\n") 
 		Summary.errorSummaryFile.write("************************************************************************\n")
-		Summary.errorSummaryFile.write("In-line errors - The following lines explain any broad geometric errors that were found while validating your parcel dataset."+ "\n")
-		if len(totError.geometricPlacementErrors) != 0:
-			for geometricErrorMessage in totError.geometricPlacementErrors:
-				Summary.errorSummaryFile.write("	General geometric error: " + str(geometricErrorMessage) + "\n")
-		if len(totError.geometricFileErrors) != 0:
-			for geometricErrorMessage in totError.geometricFileErrors:
-				Summary.errorSummaryFile.write("	General geometric error: " + str(geometricErrorMessage) + "\n")
-		if (len(totError.geometricFileErrors) == 0) and (len(totError.geometricPlacementErrors) == 0):
-			Summary.errorSummaryFile.write("	*No broad-level geometric errors found!" + "\n")
+		
 
 	def writeIniFile(self,inputDict,totError):
 		arcpy.AddMessage("Creating .ini file")
